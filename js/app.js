464faf7547a4386ecf19c2e4f19eacdc6cc0f251
JaimeenSarvaiya <jaimeen101102@gmail.com>
2024-01-30 09:41:06 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 40,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
